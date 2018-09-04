2fdbd5000e58d931e0cc0859696b75c4806d5ab7
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-09-04 13:57:48 -0400

    Added the mathTester class

---
+++ src/mathTester.java
@@ -0,0 +1,11 @@
+public class mathTester {
+
+    public static void main(String[] args) {
+        String firstWord;
+        String secondWord;
+
+        firstWord = "Dog";
+        secondWord = "House";
+        System.out.println("Test");
+    }
+}
