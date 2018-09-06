0bca075d1280261e99f1bbdcbbf4fe2a81560645
Max <Maximillian.Brown.2021@student.lhschools.org>
2018-09-06 13:00:14 -0400

    Final Version, ready to be turned in

--- src/mathTester.java
+++ src/mathTester.java
@@ -3,9 +3,22 @@ public class mathTester {
     public static void main(String[] args) {
         String firstWord;
         String secondWord;
+        int firstNumber;
+        int secondNumber;
 
         firstWord = "Dog";
         secondWord = "House";
-        System.out.println("Test");
+        firstNumber = 25;
+        secondNumber = 42;
+
+
+        System.out.println(firstNumber-secondNumber);
+        System.out.println(firstWord+(firstNumber+secondNumber));
+        System.out.println("The answer to 25 plus 42 is " + (firstNumber+secondNumber));
+        System.out.println("the answer to 25 minus 42 is " +(firstNumber-secondNumber));
+        System.out.println("The answer to 25 times 42 is " + (firstNumber*secondNumber));
+        System.out.println("42 divided by 25 is " + (secondNumber/firstNumber));
+        System.out.println("the modulus of 42 by 25 is " + (secondNumber%firstNumber));
+        System.out.println();
     }
 }
